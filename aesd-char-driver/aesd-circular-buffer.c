106862a24f785a089210e67a3f68b9f6a8d9ec9f
lijo6730 <lijo6730@likhita.myguest.virtualbox.org>
2026-02-28 12:53:36 -0700

    Assignment 7 part 1 complete

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -7,6 +7,10 @@
  * @copyright Copyright (c) 2020
  *
  */
+ 
+/*Reference :
+ChatGPT: https://chatgpt.com/share/69a34751-6abc-8009-952e-870ae8b2ee38
+*/
 
 #ifdef __KERNEL__
 #include <linux/string.h>
@@ -32,6 +36,44 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+    
+    //Checking pointer value to avoid dereferencing NULL pointer
+    if(buffer == NULL || entry_offset_byte_rtn == NULL)
+    {
+    	return NULL;
+    }
+    
+    size_t curr_pos = 0;
+    uint8_t index;
+    uint8_t count;
+    
+     /* Determine number of valid entries and starting index */
+    if (buffer->full) {
+        count = AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+        index = buffer->out_offs;   /* Oldest entry */
+    } else {
+        count = buffer->in_offs;    /* Number of valid entries */
+        index = 0;                  /* Oldest when not full */
+    }
+
+    for (uint8_t i = 0; i < count; i++) {
+
+        struct aesd_buffer_entry *entry = &buffer->entry[index];
+
+        if (char_offset < curr_pos + entry->size) {
+
+            if (entry_offset_byte_rtn)
+                *entry_offset_byte_rtn = char_offset - curr_pos;
+
+            return entry;
+        }
+
+        curr_pos = curr_pos + entry->size;
+
+        index = (index + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    /* char_offset beyond total stored data */
     return NULL;
 }
 
@@ -45,8 +87,29 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
     /**
-    * TODO: implement per description
+    * TODO: Add entry to buffer, if buffer is full it will move out offest to overwrites the older entry
     */
+    //Return, to avoid SEGV faults
+    if((buffer == NULL) || (add_entry == NULL))
+    {
+    	return;
+    }
+    
+    // 1. Add entry to buffer parameter in location buffer->in_offs
+    buffer->entry[buffer->in_offs] = *add_entry;
+
+    // increment in_offs wrap around
+    buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+
+    //2. If buffer is full, the 'out' offset must also move to skip the overwritten entry
+    if (buffer->full) {
+        buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
+    }
+
+    // 3. Flag is set to true if the buffer has become full
+    if (buffer->in_offs == buffer->out_offs) {
+        buffer->full = true;
+    }
 }
 
 /**
